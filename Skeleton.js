62d84dc61b2c79c938b48cf489de7968a0f3b673
David Hérault <dherault@gmail.com>
2020-02-17 15:06:00 +0100

    wip

---
+++ Skeleton.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+function Skeleton() {
+
+  return (
+    <View>
+      <Text>
+        Skeleton
+      </Text>
+    </View>
+  )
+}
+
+export default Skeleton
